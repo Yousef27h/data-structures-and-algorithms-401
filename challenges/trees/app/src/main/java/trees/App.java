a7cbeb6ecab0f67fef8c161d196155c8304f80bd
Yousef27h <yousef_salem97@yahoo.com>
2021-07-25 15:20:44 +0300

    max value method

--- challenges/trees/app/src/main/java/trees/App.java
+++ challenges/trees/app/src/main/java/trees/App.java
@@ -11,25 +11,27 @@ public class App {
 
     public static void main(String[] args) {
 
-//        BinaryTree<Integer> binaryTree = new BinaryTree<>();
-//
-//        binaryTree.setRoot(new Node<>(1));
-//        binaryTree.getRoot().setLeftChild(new Node<>(2));
-//        binaryTree.getRoot().setRightChild(new Node<>(3));
-//        binaryTree.getRoot().getLeftChild().setLeftChild(new Node<>(4));
-//        binaryTree.getRoot().getLeftChild().setRightChild(new Node<>(5));
-//        binaryTree.getRoot().getRightChild().setRightChild(new Node<>(6));
-//        System.out.println("In Order: "+ binaryTree.inOrder(binaryTree.getRoot()));
-//        System.out.println("Pre Order: "+binaryTree.preOrder(binaryTree.getRoot()));
-//        System.out.println("Post Order: "+binaryTree.postOrder(binaryTree.getRoot()));
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
 
-        BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
-        binarySearchTree.add(4);
-        binarySearchTree.add(3);
-        binarySearchTree.add(2);
-        binarySearchTree.add(1);
-        System.out.println(binarySearchTree.inOrder(binarySearchTree.getRoot()));
-        System.out.println(binarySearchTree.containValue(3));
+        binaryTree.setRoot(new Node<>(1));
+        binaryTree.getRoot().setLeftChild(new Node<>(2));
+        binaryTree.getRoot().setRightChild(new Node<>(3));
+        binaryTree.getRoot().getLeftChild().setLeftChild(new Node<>(4));
+        binaryTree.getRoot().getLeftChild().setRightChild(new Node<>(5));
+        binaryTree.getRoot().getRightChild().setRightChild(new Node<>(6));
+        System.out.println("In Order: "+ binaryTree.inOrder(binaryTree.getRoot()));
+        System.out.println("Pre Order: "+binaryTree.preOrder(binaryTree.getRoot()));
+        System.out.println("Post Order: "+binaryTree.postOrder(binaryTree.getRoot()));
+
+//        BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
+//        binarySearchTree.add(4);
+//        binarySearchTree.add(3);
+//        binarySearchTree.add(2);
+//        binarySearchTree.add(1);
+//        System.out.println(binarySearchTree.inOrder(binarySearchTree.getRoot()));
+//        System.out.println(binarySearchTree.containValue(3));
+
+        System.out.println(binaryTree.maxValue());
 
     }
 }
--- challenges/trees/app/src/main/java/trees/binaryTree/BinaryTree.java
+++ challenges/trees/app/src/main/java/trees/binaryTree/BinaryTree.java
@@ -46,5 +46,31 @@ public class BinaryTree<T> {
         return postArr;
     }
 
+    /**
+     * Finds the maximum value stored in the tree. Assuming that the values stored in the Binary Tree will be numeric.
+     * @return maximum number in tree
+     */
+    public int maxValue(){
+        int maxVal = Integer.MIN_VALUE;
+        Node<Integer> current = (Node<Integer>) root;
+        return traverseMax(current, maxVal);
+    }
 
+    /**
+     * Traverse through tree to find maximum number assuming all elements are integers.
+     * @param node
+     * @param maxVal
+     * @return maximum number in the tree
+     */
+    public int traverseMax(Node<Integer> node, int maxVal) {
+        if (node == null){
+            return maxVal;
+        }
+        if (node.getKey() > maxVal) {
+            maxVal = node.getKey();
+        }
+        maxVal = traverseMax(node.getLeftChild(), maxVal);
+        maxVal = traverseMax(node.getRightChild(), maxVal);
+        return maxVal;
+    }
 }
